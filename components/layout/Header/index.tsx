4de86b61a35ae0180aae817629281f01edb6309f
awwmicky <m-f-alvarez@outlook.com>
2022-11-14 22:02:28 -0800

    update header

--- components/layout/Header/index.tsx
+++ components/layout/Header/index.tsx
@@ -1,10 +1,18 @@
 // import React from 'react'
+import { Typography, IconButton } from '@material-tailwind/react'
+import { Icon } from '@/components/blocks'
 import { Layer } from '../Layer'
+
+const content = {
+	brand_name: 'Frisco Wave',
+	icon: Icon.Shopping,
+}
 
 const Header = () => (
   <header>
-    <Layer>
-      Header
+    <Layer variant="header">
+      <Typography><code>{content.brand_name}</code></Typography>
+			<IconButton variant="outlined" color="red"><content.icon size={20} /></IconButton>
     </Layer>
   </header>
 )
--- components/layout/Layer/index.tsx
+++ components/layout/Layer/index.tsx
@@ -1,9 +1,18 @@
-import { FC, PropsWithChildren } from 'react'
+import { FC, PropsWithChildren, useMemo } from 'react'
 
-interface IPLayer extends PropsWithChildren {}
+interface IPLayer extends PropsWithChildren {
+	className?: string
+	variant?: 'header' | 'footer'
+}
 
-const Layer: FC<IPLayer> = ({ children }) => (
-  <div className="container mx-auto">{ children }</div>
-)
+const Layer: FC<IPLayer> = ({ children, className, variant }) => {
+	const varOption = useMemo(() => {
+		if (variant === 'header') return 'py-2 flex place-items-center place-content-between'
+		if (variant === 'footer') return ''
+		return ''
+	}, [ variant ])
+
+	return <div className={`container mx-auto ${ className } ${ varOption }`}>{ children }</div>
+}
 
 export { Layer }
