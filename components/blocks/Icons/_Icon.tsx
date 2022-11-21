b822408de7e1ab3caafb5fdfbc37cefea68a434e
awwmicky <m-f-alvarez@outlook.com>
2022-11-21 00:25:13 -0800

    update Icons

---
+++ components/blocks/Icons/_Icon.tsx
@@ -0,0 +1,29 @@
+import {
+  AiOutlineMinus,
+  AiOutlinePlus,
+  AiOutlineLeft,
+  AiOutlineRight,
+	AiOutlineStar,
+	AiFillStar,
+  AiOutlineShopping,
+} from 'react-icons/ai'
+
+import {
+  VscClose,
+} from 'react-icons/vsc'
+
+import {
+	BsSoundwave,
+	BsBagCheckFill,
+} from 'react-icons/bs'
+
+export const BrandLogo = () => <BsSoundwave size={20} strokeWidth={0.25} />
+export const Plus = () => <AiOutlinePlus />
+export const Minus = () => <AiOutlineMinus />
+export const Remove = () => <VscClose size={20} />
+export const LeftArrow = () => <AiOutlineLeft size={14} />
+export const RightArrow = () => <AiOutlineRight size={14} color="gray" />
+export const StarEmpty = () => <AiOutlineStar color="red" />
+export const StarFilled = () => <AiFillStar color="red" />
+export const Shopping = () => <AiOutlineShopping size={20} />
+export const CheckoutSuccess = () => <BsBagCheckFill size={40} color="green" />
