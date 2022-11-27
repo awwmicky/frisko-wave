a876e8f6add5afd367f8458659e74e2845d70e8f
awwmicky <m-f-alvarez@outlook.com>
2022-11-26 22:14:33 -0800

    update environment

--- src/lib/_logger.ts
+++ src/lib/_logger.ts
@@ -1,13 +1,15 @@
-import pino from 'pino'
+// import pino from 'pino'
 
-export const logger = pino({
-  level: 'info',
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      colorize: true,
-      translateTime: "SYS:dd-mm-yyyy HH:MM:ss",
-      ignore: "pid,hostname",
-    },
-  },
-})
+// export const logger = pino({
+//   level: 'debug',
+//   transport: {
+//     target: 'pino-pretty',
+//     options: {
+//       colorize: true,
+//       translateTime: "SYS:dd-mm-yyyy HH:MM:ss",
+//       ignore: "pid,hostname",
+//     },
+//   },
+// })
+
+export const logger: Console = console
